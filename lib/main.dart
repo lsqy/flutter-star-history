85842c00eda6d47cde152a6a8d739e20df922f49
lsqy <lsqy93@gmail.com>
2020-01-11 22:37:22 +0800

    feat(core): study widget

--- lib/main.dart
+++ lib/main.dart
@@ -121,6 +121,30 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: Text('ContextRoute')
             ),
+            RaisedButton(
+              onPressed: () async {
+                Navigator.push( context,
+                  new MaterialPageRoute(builder: (context) {
+                    return TapboxA();
+                  }, 
+                  maintainState: true,
+                  fullscreenDialog: true)
+                );
+              },
+              child: Text('tabboxA')
+            ),
+            RaisedButton(
+              onPressed: () async {
+                Navigator.push( context,
+                  new MaterialPageRoute(builder: (context) {
+                    return ParentWidget();
+                  }, 
+                  maintainState: true,
+                  fullscreenDialog: true)
+                );
+              },
+              child: Text('tabboxB')
+            ),
           ],
         ),
       ),
@@ -229,6 +253,104 @@ class ContextRoute extends StatelessWidget {
           Scaffold scaffold = context.findAncestorWidgetOfExactType();
           return (scaffold.appBar as AppBar).title;
         }),
+      ),
+    );
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => new _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            _active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+// ParentWidget 为 TapboxB 管理状态.
+
+//------------------------ ParentWidget --------------------------------
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => new _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+//------------------------- TapboxB ----------------------------------
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
       ),
     );
   }

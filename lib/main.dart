2c4311f482b5ad7affc9250aee4e64b20a0416f9
lsqy <lsqy93@gmail.com>
2020-01-28 19:50:05 +0800

    study basic

--- lib/main.dart
+++ lib/main.dart
@@ -193,6 +193,50 @@ class _MyHomePageState extends State<MyHomePage> {
                 value: .7,
               ),
             ),
+            Column(
+              //测试Row对齐方式，排除Column默认居中对齐的干扰
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(" hello world "),
+                    Text(" I am Jack "),
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(" hello world "),
+                    Text(" I am Jack "),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  textDirection: TextDirection.rtl,
+                  children: <Widget>[
+                    Text(" hello world "),
+                    Text(" I am Jack "),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,  
+                  verticalDirection: VerticalDirection.up,
+                  children: <Widget>[
+                    Text(" hello world ", style: TextStyle(fontSize: 30.0),),
+                    Text(" I am Jack "),
+                  ],
+                ),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("hi"),
+                Text("world"),
+              ],
+            )
           ],
         ),
       ),
